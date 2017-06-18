5bde38288b2c1315b6a21ab0f5ab07efb386f3c6
win-s <desperandro@gmail.com>
2017-06-18 22:41:43 +0700

    drag and drop task

--- app/app.js
+++ app/app.js
@@ -3,10 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('app', [
   'ngRoute',
-  'app.todolist'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  'app.todolist',
+  'dndLists'// drag & drop list from angular-drag-and-drop-lists libary
+])
+.config(config);
+
+function config($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/todolist'});
-}]);
+}
--- app/todolist/todolist_ctrl.js
+++ app/todolist/todolist_ctrl.js
@@ -12,27 +12,32 @@ function TodolistPageCtrl($scope){
 
 }
 
-function BaseTodolistCtrl($scope){
+function BaseTodolistCtrl($scope,$rootScope){
   $scope.tasks = [];
   $scope.edit = false;
-  $scope.startEdit = function(){
-    $scope.edit = true;
+
+  $scope.$on("event:onEdit",function(event,isEdit){
+    $scope.edit = isEdit;
+  });
+  $scope.onEdit = function(isEdit){
+    $rootScope.$broadcast("event:onEdit",isEdit);
   };
-  $scope.onDone = function(){
-    $scope.edit = false;
-  };
+  
   $scope.storeModel = function(){};// overloadding this function
 
   $scope.onFinishTask = function(item){
     if(!$scope.edit){
       item.finished = !item.finished;
     }
-
   };
   $scope.onRemove = function(item){
     $scope.remove();
   };
   $scope.remove = function(item){
+    var index = $scope.tasks.indexOf(item);
+    $scope.tasks.splice(index,1);
+  }
+  $scope.onMoved = function(item){
     var index = $scope.tasks.indexOf(item);
     $scope.tasks.splice(index,1);
   }
@@ -42,6 +47,7 @@ function TodayCtrl($scope,$controller,TodayService,TomorrowService){
   $controller('BaseTodolistCtrl', {$scope: $scope});
   $scope.tasks = TodayService.getModel();
   $scope.task ="";
+  $scope.todayDate = moment().format("MMM D YYYY").toString();
   
   $scope.addTask = function(task){
     $scope.tasks.unshift({
@@ -50,7 +56,7 @@ function TodayCtrl($scope,$controller,TodayService,TomorrowService){
     });
 
     $scope.task = "";
-    $scope.edit = false;
+    $scope.onEdit(false);
 
   }
 
@@ -64,14 +70,14 @@ function TodayCtrl($scope,$controller,TodayService,TomorrowService){
 }
 
 function TomorrowCtrl($scope,$controller,TomorrowService,TodayService){
-  $controller('BaseTodolistCtrl', {$scope: $scope});
-  $scope.tasks = TomorrowService.getModel();
+    $controller('BaseTodolistCtrl', {$scope: $scope});
+    $scope.tasks = TomorrowService.getModel();
+    $scope.tomorrowDate = moment().add(1,'days').format("MMM D YYYY").toString();
+    $scope.moveToToday = function(item){
 
-  $scope.moveToToday = function(item){
+      var todayModel= TodayService.getModel();
+      todayModel.push(item);
 
-    var todayModel= TodayService.getModel();
-    todayModel.push(item);
-
-    $scope.remove(item);
-  }  
-}
+      $scope.remove(item);
+    }
+}  
